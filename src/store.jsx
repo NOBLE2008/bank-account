f28a49d102008efea3d81762b3ad13b74aba883d
Noble Benjamin <nobleben2008@gmail.com>
2024-08-04 18:26:04 +0100

    Added Account Operations,jsx

--- src/store.jsx
+++ src/store.jsx
@@ -1,10 +1,9 @@
+import { createStore } from "redux";
 const initialState = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
 };
-
-
 
 function reducer(state = initialState, action) {
   switch (action.type) {
@@ -14,22 +13,29 @@ function reducer(state = initialState, action) {
         balance: state.balance + action.payload,
       };
     case "account/withdraw":
-        return {
-            ...state,
-            balance: state.balance - action.payload,
-        }
+      return {
+        ...state,
+        balance: state.balance - action.payload,
+      };
     // LATER
     case "account/requestLoan":
-        if(state.loan > 0) return state
-        return {
-            ...state,
-            loan: action.payload,
-        }
-  case "account/payLoan":
-    return {
+      if (state.loan > 0) return state;
+      return {
+        ...state,
+        loan: action.payload,
+      };
+    case "account/payLoan":
+      return {
         ...state,
         balance: state.balance + action.payload,
         loan: 0,
         loanPurpose: "",
-    }
+      };
+    default:
+      return state;
+  }
 }
+
+const store = createStore(reducer);
+
+store.dispatch({type: "account/deposit", payload: 500})
